923764d051860ec99db7a14d44bc8fb4b340df68
Nam200700 <tranhainam200700@gmail.com>
2026-06-08 15:07:44 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,23 @@ import AppRoutes from './routes';
 import RoleSwitcher from './components/widget/RoleSwitcher';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { WebSocketProvider } from './contexts/WebSocketContext';
+import { ErrorBoundary } from './components/common/ErrorBoundary';
 
 function App() {
   return (
-    <ThemeProvider>
-      <WebSocketProvider>
-        <BrowserRouter>
-          {/* Ứng dụng chính */}
-          <AppRoutes />
+    <ErrorBoundary>
+      <ThemeProvider>
+        <WebSocketProvider>
+          <BrowserRouter>
+            {/* Ứng dụng chính */}
+            <AppRoutes />
 
-          {/* Floating Demo Tool để chuyển vai trò cực kỳ tiện lợi */}
-          <RoleSwitcher />
-        </BrowserRouter>
-      </WebSocketProvider>
-    </ThemeProvider>
+            {/* Floating Demo Tool để chuyển vai trò cực kỳ tiện lợi */}
+            <RoleSwitcher />
+          </BrowserRouter>
+        </WebSocketProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
 
